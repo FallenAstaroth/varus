433c807152feef115171b8e2525c7324f949e322
FallenAstaroth <andryvorobey123@gmail.com>
2023-07-09 22:17:11 +0300

    404 page added

---
+++ frontend/src/components/404.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="error-page">
+    <div class="wrapper">
+      <div class="error">
+        <div class="image">
+          <img src="@/assets/img/404.png" alt="">
+        </div>
+        <p>{{ error }}</p>
+        <a href="/" class="btn btn-primary btn-home">{{ button }}</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Page404Component",
+  data() {
+    return {
+      error: "Looks like you're lost",
+      button: "Back to Home"
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+body {
+  position: relative;
+}
+
+.error-page {
+  height: 100%;
+
+  .wrapper {
+    width: 100%;
+    max-width: 1920px;
+    margin: 0 auto;
+    padding: 20px;
+    display: grid;
+    place-items: center;
+    overflow-y: auto;
+    box-sizing: border-box;
+  }
+
+  .error {
+    width: 100%;
+    max-width: max-content;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    .image {
+      width: 100%;
+      max-width: 256px;
+
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+      }
+    }
+
+    p {
+      font-weight: 700;
+      color: #cbcbe2;
+      font-size: 1.375rem;
+      text-align: center;
+    }
+
+    .btn-home {
+      margin: 20px auto;
+      text-decoration: none;
+    }
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .error-page {
+    .error {
+      .image {
+        width: 100%;
+        max-width: 200px;
+      }
+
+      p {
+        font-size: 1rem;
+      }
+    }
+  }
+}
+</style>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,12 +1,14 @@
 import {createRouter, createWebHistory} from "vue-router";
 import Index from "@/components/index.vue";
 import Room from "@/components/room.vue";
+import Error from "@/components/404.vue";
 
 
 export default createRouter({
     history: createWebHistory(),
     routes: [
-        { path: "/", component: Index },
-        { path: "/room/:roomId", component: Room }
+        { name: "Index", path: "/", component: Index },
+        { name: "Room", path: "/room/:roomId", component: Room },
+        { name: "Error", path: "/:pathMatch(.*)", component: Error }
     ]
 })
