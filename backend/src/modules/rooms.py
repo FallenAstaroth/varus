4dab3ccf02af8eb9fba3aba8efa14b40cddc3bb3
FallenAstaroth <andryvorobey123@gmail.com>
2023-09-11 22:01:17 +0300

    Player updated

---
+++ backend/src/modules/rooms.py
@@ -0,0 +1,80 @@
+from typing import Union
+from random import choice
+from string import ascii_letters, digits
+
+
+class RoomsManager:
+
+    def __init__(self):
+        self.__rooms = {}
+        self.__symbols = ascii_letters + digits
+
+    @property
+    def rooms(self) -> dict:
+        return self.__rooms
+
+    async def __generate_room_code(self, length: int) -> str:
+        """
+        Generates a unique room code.
+
+        Parameters:
+        - length [int]: Length of generated code.
+
+        Returns:
+        - str: Unique room code.
+        """
+        while True:
+            code = ''.join(choice(self.__symbols) for _ in range(length))
+
+            if code in self.__rooms:
+                continue
+
+            return code
+
+    async def create_room(self, videos: str) -> str:
+        """
+        Creates a new room.
+
+        Parameters:
+        - videos [str]: Videos of the current room in PlayerJs str format.
+
+        Returns:
+        - str: Code of the created room.
+        """
+        code = await self.__generate_room_code(10)
+
+        self.__rooms.update({
+            code: {
+                "count": 0,
+                "videos": videos,
+                "last_message_id": 1,
+                "languages": set()
+            }
+        })
+
+        return code
+
+    async def get_room(self, code: str) -> Union[dict, None]:
+        """
+        Returns the room data by room code.
+
+        Parameters:
+        - code [str]: Room code.
+
+        Returns:
+        - dict: Room data.
+        """
+        return self.__rooms.get(code, None)
+
+    async def check_room(self, code: str) -> bool:
+        """
+        Checks if the room exists by its code.
+
+        Parameters:
+        - code [str]: Room code.
+
+        Returns:
+        - bool: True if it exists, False if it doesn't.
+        """
+        room = self.__rooms.get(code, None)
+        return True if room else False
