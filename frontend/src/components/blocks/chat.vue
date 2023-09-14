3095df0ca221e3788d513044ebcff26dcc14e981
FallenAstaroth <andryvorobey123@gmail.com>
2023-09-14 20:42:39 +0300

    Attachment updated

--- frontend/src/components/blocks/chat.vue
+++ frontend/src/components/blocks/chat.vue
@@ -280,7 +280,8 @@ export default {
     left: 0;
     width: 100%;
     height: 100%;
-    background: rgba(0, 0, 0, .6);
+    background: rgba(0, 0, 0, .5);
+    z-index: 100;
 
     &.hidden {
       display: none;
